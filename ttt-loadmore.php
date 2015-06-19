a2c02fda83400391d2e937574f04ec78d786c42e
Gabriel Pérez S <gabriel@guero.net>
2015-06-19 13:30:40 +0200

    readme and version

--- ttt-loadmore.php
+++ ttt-loadmore.php
@@ -9,42 +9,40 @@ Author URI: http://www.33themes.com
 */
 
 
-
-
-
 define('TTTINC_LOADMORE', dirname(__FILE__) );
-define('TTTVERSION_LOADMORE', 0.1 );
+define('TTTVERSION_LOADMORE', 1.0 );
 
 
 function ttt_autoload_loadmore( $class ) {
-	if ( 0 !== strpos( $class, 'TTTLoadmore_' ) )
-		return;
-	
-	$file = TTTINC_LOADMORE . '/class/' . $class . '.php';
-	if (is_file($file))
-		require_once $file;
-		return true;
-	
-	throw new Exception("Unable to load $class at ".$file);
+    if ( 0 !== strpos( $class, 'TTTLoadmore_' ) )
+        return;
+    
+    $file = TTTINC_LOADMORE . '/class/' . $class . '.php';
+    if (is_file($file)) {
+        require_once $file;
+        return true;
+    }
+    
+    throw new Exception("Unable to load $class at ".$file);
 }
 
 if ( function_exists( 'spl_autoload_register' ) ) {
-	spl_autoload_register( 'ttt_autoload_loadmore' );
+    spl_autoload_register( 'ttt_autoload_loadmore' );
 } else {
-	require_once TTTINC_LOADMORE . '/class/TTTLoadmore_Common.php';
+    require_once TTTINC_LOADMORE . '/class/TTTLoadmore_Common.php';
 }
 
 function tttloadmore_init () {
- 	$s = load_plugin_textdomain( 'tttloadmore', false, dirname( plugin_basename( __FILE__ ) ) . '/lang/' );
-	if ( !is_admin() ) {
-		global $TTTLoadmore_Front;
-		$TTTLoadmore_Front = new TTTLoadmore_Front();
-		$TTTLoadmore_Front->init();
-	}
-	else {
-		$TTTLoadmore_Admin = new TTTLoadmore_Admin();
-		$TTTLoadmore_Admin->init();
-	}
+    $s = load_plugin_textdomain( 'tttloadmore', false, dirname( plugin_basename( __FILE__ ) ) . '/lang/' );
+    if ( !is_admin() ) {
+        global $TTTLoadmore_Front;
+        $TTTLoadmore_Front = new TTTLoadmore_Front();
+        $TTTLoadmore_Front->init();
+    }
+    else {
+        $TTTLoadmore_Admin = new TTTLoadmore_Admin();
+        $TTTLoadmore_Admin->init();
+    }
 }
 
 add_action('init', 'tttloadmore_init', 10);
